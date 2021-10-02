1717a927b0ba0dd2b2e9a11613bdebdbbf9e4eef
MorphTail <chitniskshitij@gmail.com>
2021-10-02 18:58:21 +0530

    moving ball

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use nannou::prelude::*;
+
+struct Model {}
+
+fn main() {
+    nannou::app(model).event(event).simple_window(view).run();
+}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    let sine = app.time.sin();
+    let slowersine = (app.time / 2.0).sin();
+    let boundary = app.window_rect();
+
+    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
+    let y = map_range(slowersine, -1.0, 1.0, boundary.bottom(), boundary.top());
+
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEELBLUE).x_y(x, y);
+    draw.to_frame(app, &frame).unwrap();
+}
